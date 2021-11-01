34b6e02e0a84e3ada9dd92beeee73b597fb2deff
Tom Officer <thomasofficerjr@gmail.com>
2021-11-01 00:36:11 -0700

    added contact buttons, added contact badges

--- client/src/components/contacts/ContactItem.js
+++ client/src/components/contacts/ContactItem.js
@@ -1,4 +1,4 @@
-//8 mins into #53!!!!
+import PropTypes from 'prop-types'
 
 
 const ContactItem = ({ contact }) => {
@@ -8,18 +8,35 @@ const ContactItem = ({ contact }) => {
         <div className="card bg-light">
             <h3 className="text-primary text-left">
                 {name}{' '}
-                <span
+                <span style={{ float: 'right' }}
                     className={
                         'badge ' +
                         (type === 'professional' ? 'badge-success' : 'badge-primary')
                     }
                 >
-                    {type}
+                    {type.charAt(0).toUpperCase() + type.slice(1)}
                 </span>
             </h3>
+            <ul className="list">
+                {email && (<li>
+                    <i className="fas fa-envelope-open" /> {email}
+                </li>)}
+                {phone && (<li>
+                    <i className="fas fa-phone" /> {phone}
+                </li>)}
+            </ul>
+
+            <p>
+                <button className="btn btn-dark btn-sm">Edit</button>
+                <button className="btn btn-danger btn-sm">Delete</button>
+            </p>
             
         </div>
     )
 } 
+
+ContactItem.propTypes = {
+    contact: PropTypes.object.isRequired,
+}
 
 export default ContactItem;
