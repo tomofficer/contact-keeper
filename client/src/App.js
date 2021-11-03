889ae4335ed124507258b706f520ee33b9984491
Tom Officer <thomasofficerjr@gmail.com>
2021-11-03 00:19:42 -0700

    added register page

--- client/src/App.js
+++ client/src/App.js
@@ -4,25 +4,31 @@ import { Navbar } from './components/layout/Navbar';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './components/pages/Home';
 import About from './components/pages/About';
+import Register from './components/auth/Register';
+
 import ContactState from './context/contact/ContactState';
+import AuthState from './context/auth/AuthState';
 
 
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-            <Fragment>
-              <Navbar />
-              <div className="container">
-                <Switch>
-                  <Route exact path='/' component={Home}/>
-                  <Route exact path='/about' component={About}/>
-                </Switch>
-              </div>
-          </Fragment>
-        </Router>
-      </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+              <Fragment>
+                <Navbar />
+                <div className="container">
+                  <Switch>
+                    <Route exact path='/' component={Home}/>
+                    <Route exact path='/about' component={About}/>
+                    <Route exact path='/register' component={Register}/>
+                  </Switch>
+                </div>
+            </Fragment>
+          </Router>
+        </ContactState>
+      </AuthState>
   );
 }
 
