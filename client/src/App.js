452719a2660e863c426a4fffc1a9dc67cf85ec70
Tom Officer <thomasofficerjr@gmail.com>
2021-11-03 19:16:22 -0700

    added alert state, alert context and Alert component

--- client/src/App.js
+++ client/src/App.js
@@ -9,6 +9,7 @@ import Login from './components/auth/Login';
 
 import ContactState from './context/contact/ContactState';
 import AuthState from './context/auth/AuthState';
+import AlertState from './context/alert/AlertState';
 
 
 
@@ -16,19 +17,21 @@ const App = () => {
   return (
     <AuthState>
       <ContactState>
-        <Router>
-              <Fragment>
-                <Navbar />
-                <div className="container">
-                  <Switch>
-                    <Route exact path='/' component={Home}/>
-                    <Route exact path='/about' component={About}/>
-                    <Route exact path='/register' component={Register}/>
-                    <Route exact path='/login' component={Login}/>
-                  </Switch>
-                </div>
-            </Fragment>
-          </Router>
+        <AlertState>
+          <Router>
+                <Fragment>
+                  <Navbar />
+                  <div className="container">
+                    <Switch>
+                      <Route exact path='/' component={Home}/>
+                      <Route exact path='/about' component={About}/>
+                      <Route exact path='/register' component={Register}/>
+                      <Route exact path='/login' component={Login}/>
+                    </Switch>
+                  </div>
+              </Fragment>
+            </Router>
+          </AlertState>
         </ContactState>
       </AuthState>
   );
---
+++ client/src/components/layout/Alerts.js
@@ -0,0 +1,14 @@
+import { useContext } from 'react';
+import AlertContext from '../../context/alert/alertContext';
+
+const Alerts = () => {
+
+    const alertContext = useContext(AlertContext);
+    return (
+        <div>
+            
+        </div>
+    )
+}
+
+export default Alerts
